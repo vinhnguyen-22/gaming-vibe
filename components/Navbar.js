c280623c413a5fe65ceb6f78cad2c75376827ae7
vinhnguyen-22 <vinhnguyenad22@gmail.com>
2021-06-27 11:25:51 +0700

    1

---
+++ components/Navbar.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <div className="container">
+      <nav>
+        <Image src="/rupee.png" width={50} height={48} />
+        <h1>Gaming Vibes</h1>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/guides">
+              <a>Guides</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="banner">
+        <Image src="/banner-gaming.jpg" width={966} height={476} />
+      </div>
+    </div>
+  );
+}
